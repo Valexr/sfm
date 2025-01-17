90f204fd42ee63b937ec61b94646016edc8e6f6b
Valexr <valexr@gmail.com>
2025-01-17 21:39:26 +0200

    simplify types

--- src/global.d.ts
+++ src/global.d.ts
@@ -7,26 +7,15 @@ interface ChannelType {
     id: "7soul",
     title: "Seven Inch Soul",
     description: "Vintage soul tracks from the original 45 RPM vinyl.",
-    dj: "Dion Watts Garcia",
-    djmail: "dion@somafm.com",
-    genre: "oldies",
     img: string,
     bg: string
-    twitter: "",
-    updated: "1396144686",
     playlists: Array<Playlist>,
-    preroll: [],
-    listeners: "60",
-    lastPlaying: "Yvonne Fair And The James Brown Band - Tell Me Why",
-    src: string,
     song: SongType,
 }
 
 interface Playlist {
     url: string,
     src: string
-    format: "mp3" | "aac" | "aacp",
-    quality: "highest" | "high" | "low",
     title: string
 }
 
