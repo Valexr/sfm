6bb4d62c2ae75b75e3290b583eec17ebed2886a5
Valexr <valexr@gmail.com>
2026-07-12 13:07:26 +0000

    Deploying to gh-pages from @ Valexr/sfm@94d9419f4d7796783708296d2ba88f094f855535 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (_event) => {
   console.log('Service Worker установлен');
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', (_event) => {
   console.log('Service Worker активирован');
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (_event) => {
   // Можно добавить кэширование
 });
